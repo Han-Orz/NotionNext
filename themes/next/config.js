00350016a0a6a28aac579c814848947bd0b83bc2
Han <han.orz@qq.com>
2023-07-24 22:28:59 +0800

    打开搜索

--- themes/next/config.js
+++ themes/next/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: false, // 显示搜索
+  MENU_SEARCH: true, // 显示搜索
 
   WIDGET_TO_TOP: true, // 是否显示回顶
   WIDGET_TO_BOTTOM: false, // 显示回底
